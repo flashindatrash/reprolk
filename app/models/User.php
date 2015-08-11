da308d9957ef89fad3e98f517bdfe2199b36c28d
iSeeYou87 <job@alphacrust.ru>
2015-08-11 13:01:42 +0000

    User.php created online with Bitbucket

---
+++ app/models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+class UserData extends BaseModel {
+
+	protected $id;
+	protected $group;
+	protected $email;
+	protected $password;
+	protected $firstname;
+	protected $lastname;
+
+}
+
+?>
