39c47a4295f68ce353aacd7e5934e0b17de1c0ef
iSeeYou87 <job@alphacrust.ru>
2016-08-03 00:18:12 +0300

    add AjaxRoute

---
+++ core/objects/route/Route.php
@@ -0,0 +1,123 @@
+<?php
+
+class Route {
+	
+	const INDEX = 'Index';
+	const FILE = 'File';
+	const LOGIN = 'Login';
+	const LOGOUT = 'Logout';
+	const NOT_FOUND = 'NotFound';
+	const ACCESS_DENIED = 'AccessDenied';
+	const ORDER_ADD = 'OrderAdd';
+	const ORDER_VIEW = 'OrderView';
+	const ORDER_EDIT = 'OrderEdit';
+	const ORDER_CANCEL = 'OrderCancel';
+	const ORDER_DELETE = 'OrderDelete';
+	const ORDER_DUPLICATE = 'OrderDuplicate';
+	const ORDER_ALL = 'OrderAll';
+	const ORDER_REPEAT = 'OrderRepeat';
+	const ORDER_ARCHIVE = 'OrderArchive';
+	const ORDER_APPROVAL = 'OrderApproval';
+	const ORDER_APPROVED = 'OrderApproved';
+	const ORDER_DISAPPROVED = 'OrderDisapproved';
+	const POLYMER_ALL = 'PolymerAll';
+	const POLYMER_BIND = 'PolymerBind';
+	const POLYMER_DELETE = 'PolymerDelete';
+	const PROFILE = 'User';
+	const USER_ADD = 'UserAdd';
+	const USER_ALL = 'UserAll';
+	const TRANSMIT_RIGHTS = 'TransmitRights';
+	const ADMIN = 'Admin';
+	const VIEW_AS = 'ViewAs';
+	const VIEW_AS_CANCEL = 'ViewAsCancel';
+	const COMMENT_DELETE = 'CommentDelete';
+	const FIELD_PAGES = 'FieldPages';
+	const FIELD_PAGE = 'FieldPage';
+	const FIELD_DELETE = 'FieldDelete';
+	const FIELD_ADD = 'FieldAdd';
+	const FIELD_BIND = 'FieldBind';
+	const TEMPLATE_VIEW = 'TemplateView';
+	const TEMPLATE_EDIT = 'TemplateEdit';
+	const TEMPLATE_DELETE = 'TemplateDelete';
+	const LOCALE_ALL = 'LocaleAll';
+	const LOCALE_STATS = 'LocaleStats';
+	const LOCALE_EDIT = 'LocaleEdit';
+	const LANGUAGE_SET = 'LanguageSet';
+	const API_DOCUMENTATION = 'ApiDocumentation';
+	const CRON = 'Cron';
+	const SWITCH_PLUGIN = 'SwitchPlugin';
+	const API_LOGIN = 'ApiLogin';
+	const API_ORDER_ADD = 'ApiOrderAdd';
+	const API_ORDER_ALL = 'ApiOrderAll';
+	const API_ORDER_CANCEL = 'ApiOrderCancel';
+	const API_ORDER_REPEAT = 'ApiOrderRepeat';
+	const API_FIELD_GET = 'ApiFieldGet';
+	
+	const TYPE_NORMAL = 0;
+	const TYPE_SUB = 1;
+	const TYPE_HIDDEN = 2;
+	
+	public $name;
+	public $path;
+	public $permission;
+	public $type;
+	public $routes;
+	
+	public function __construct($name, $path = '/', $permission = null, $type = 0, $routes = null) {
+		$this->name = $name;
+		$this->path = $path;
+		$this->permission = $permission;
+		$this->type = $type;
+		$this->routes = is_null($routes) ? [] : $routes;
+	}
+	
+	//клонирование роута
+	public function __clone() {
+		return new Route($this->name, $this->path, $this->permission, $this->type);
+	}
+	
+	//текст для ссылок
+	public function linkText() {
+		return Application::str('menu_' . $this->name);
+	}
+	
+	//текст в хлебных крошках
+	public function breadcrumpText() {
+		return $this->linkText();
+	}
+	
+	//текст в ссылках при наведении
+	public function linkTitle() {
+		return Application::str('menu_title_' . $this->name);
+	}
+	
+	//доступно ли
+	public function isAvailable() {
+		return is_null($this->permission) || UserAccess::check($this->permission);
+	}
+	
+	//виден ли в меню
+	public function isVisible() {
+		return $this->type!=Route::TYPE_HIDDEN && $this->isAvailable();
+	}
+	
+	//путь контроллера
+	public function controllerPath() {
+		return $this->controllerName() . '.php';
+	}
+	
+	//имя контроллера
+	public function controllerName() {
+		return $this->name . 'Controller';
+	}
+	
+	//вернуть путь с GET'ом
+	public function forGet($get) {
+		return $this->path . (!is_null($get) ? '?' . $get : '');
+	}
+	
+	public static function byName($name) {
+		return Application::$routes->byName($name);
+	}
+	
+}
---
+++ core/objects/route/Routes.php
@@ -0,0 +1,69 @@
+<?php
+
+class Routes {
+	
+	private $dictByName = array();
+	private $dictByPath = array();
+	
+	public $all;
+	public $current;
+	
+	public function __construct($array) {
+		$this->all = $array;
+		$this->parse($this->all);
+		$this->current = $this->getCurrent();
+	}
+	
+	public function byPath($path) {
+		return array_key_exists($path, $this->dictByPath) ? $this->dictByPath[$path] : null;
+	}
+	
+	public function byName($name) {
+		return array_key_exists($name, $this->dictByName) ? $this->dictByName[$name] : null;
+	}
+	
+	public function equalRoute($route) {
+		return !is_null($this->current) && $this->current->name == $route->name;
+	}
+	
+	public function currentPath() {
+		return !is_null($this->current) ? $this->current->path : '';
+	}
+	
+	public function currentGet() {
+		return get('_url');
+	}
+	
+	//поиск роутов по их классу
+	public function findByClass($class) {
+		$ret = array();
+		foreach ($this->all as $route) {
+			if ($route instanceof $class) {
+				$ret[] = $route;
+			}
+		}
+		return $ret;
+	}
+	
+	public function parseUrl($url) {
+		$u = strtolower($url);
+		$delim = strpos($u, '?');
+		if ($delim!==false) $u = substr($u, 0, $delim);
+		return $this->byPath($u);
+	}
+	
+	private function parse($routes) {
+		foreach ($routes as $route) {
+			$this->dictByName[$route->name] = $route;
+			$this->dictByPath[$route->path] = $route;
+			$this->parse($route->routes);
+		}
+	}
+	
+	private function getCurrent() {
+		if (!hasGet('_url')) return $this->byName(Route::ORDER_ALL);
+		return $this->parseUrl($this->currentGet());
+	}
+}
+
+?>
