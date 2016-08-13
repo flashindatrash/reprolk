f7bcaaad063c13d19d0c27c3c91bb6d97e9846b1
iSeeYou87 <merzavec87@inbox.ru>
2016-08-13 15:50:40 +0300

    fix высоты текстового поля в ответе от АПИ

--- public/js/controller/ApiExecute.js
+++ public/js/controller/ApiExecute.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
 	
+	//текстовое поле
 	var input_response = $('#input_response');
 	
+	//url метода
+	var path = params['method_path'];
+	
+	//выставим текстовое поле только для чтения
 	input_response.prop('readonly', true);
 	
 	$('#execute').click(function () {
 		var form = $(this).closest('form');
 		if (form) {
+			//сериализуем форму в объект
 			var data = form.serializeArray();
-			ajax('/api/login', data, function (result) { 
-				
+			ajax(path, data, function (result) { 
+				//распарсим JSON для чтения
 				var response = JSON.stringify(result, null, '\t');
-				var rows = response.split('\t').length;
+				//посчитаем сколько строк, чтобы расширить текстовое поле
+				var rows = response.split(/\r\n|\r|\n/).length;
 				
 				input_response.val(response);
-				input_response.attr('rows', rows+1);
-				//input_response.prop('height', input_response.prop('scrollHeight') + 'px');
+				input_response.attr('rows', rows);
 			});
 		}
 	});
